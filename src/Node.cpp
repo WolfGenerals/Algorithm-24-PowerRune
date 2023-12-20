030f9ba51d180d3669ed9abd8a0f2654cbcc0d29
WolfGenerals <2301146268@qq.com>
2023-12-21 00:19:09 +0800

    改成ROS项目

---
+++ src/Node.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "Alias.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world power_rune package\n");
+  return 0;
+}
--- src/Node.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef NODE_HPP
-#define NODE_HPP
-
-
-class Node : public rclcpp::Node {
-};
-
-
-#endif//NODE_HPP
