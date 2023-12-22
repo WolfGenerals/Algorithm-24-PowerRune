897fd31f436fff2a48d2f890c889b3e0b76835fa
WolfGenerals <2301146268@qq.com>
2023-12-22 21:45:34 +0800

    使用参数服务传参

--- src/PowerRuneNode.cpp
+++ src/PowerRuneNode.cpp
@@ -2,8 +2,8 @@
 #include "Alias.hpp"
 #include "Converter/Transform.hpp"
 #include "cv_bridge/cv_bridge.h"
-#include "geometry_msgs/msg/polygon_stamped.hpp"
 #include "geometry_msgs/msg/point_stamped.hpp"
+#include "geometry_msgs/msg/polygon_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
 
@@ -13,11 +13,65 @@ using namespace rclcpp;
 
 class PowerRuneNode final : public Node {
     shared_ptr<Feature> sourceFeature;
-    std::vector<Vec3>   sourceWorldPoints;
-    std::vector<Vec2>   sourceImagePoints;
 
     cv::Matx33f           cameraMatrix;
     cv::Matx<float, 1, 5> distCoeffs;
+
+    std::vector<Vec3> world_points() const {
+        vector<Vec3>  points;
+        vector<double> rawData;
+
+        get_parameter("world_points", rawData);
+        if (rawData.empty())
+            RCLCPP_ERROR(get_logger(), "world_points is empty");
+        if (rawData.size() % 3!= 0)
+            RCLCPP_ERROR(get_logger(), "world_points size error");
+
+        for (int i = 0; i < rawData.size(); i += 3) {
+            points.emplace_back(rawData[i], rawData[i + 1], rawData[i + 2]);
+        }
+        return move(points);
+    };
+
+    std::vector<Vec2> image_points() const {
+        vector<Vec2>  points;
+        vector<double> rawData;
+
+        get_parameter("image_points", rawData);
+        if (rawData.empty())
+            RCLCPP_ERROR(get_logger(), "image_points is empty");
+        if (rawData.size() % 2!= 0)
+            RCLCPP_ERROR(get_logger(), "image_points size error");
+
+        for (int i = 0; i < rawData.size(); i += 2) {
+            points.emplace_back(rawData[i], rawData[i + 1]);
+        }
+        return move(points);
+    };
+
+    Vec3 world_target() const {
+        vector<double> rawData;
+
+        get_parameter("world_target", rawData);
+        if (rawData.empty())
+            RCLCPP_ERROR(get_logger(), "world_target is empty");
+        if (rawData.size()!= 3)
+            RCLCPP_ERROR(get_logger(), "world_target size error");
+
+        return {static_cast<float>(rawData[0]), static_cast<float>(rawData[1]), static_cast<float>(rawData[2])};
+    };
+
+    Vec3 world_center() const {
+        vector<double> rawData;
+
+        get_parameter("world_center", rawData);
+        if (rawData.empty())
+            RCLCPP_ERROR(get_logger(), "world_center is empty");
+        if (rawData.size()!= 3)
+            RCLCPP_ERROR(get_logger(), "world_center size error");
+
+        return {static_cast<float>(rawData[0]), static_cast<float>(rawData[1]), static_cast<float>(rawData[2])};
+    };
 
 
     Publisher<geometry_msgs::msg::PolygonStamped>::SharedPtr rune_in_camera_publisher;
@@ -26,7 +80,8 @@ class PowerRuneNode final : public Node {
     Publisher<geometry_msgs::msg::PointStamped>::SharedPtr   prediction_in_camera_publisher;
     Subscription<sensor_msgs::msg::Image>::SharedPtr         image_subscriber;
 
-    void main(const sensor_msgs::msg::Image::SharedPtr& imageRos) {
+
+    void main(const sensor_msgs::msg::Image::SharedPtr& imageRos) const {
         const cv_bridge::CvImageConstPtr image = cv_bridge::toCvShare(imageRos);
         if (image->image.empty()) {
             RCLCPP_WARN(get_logger(), "image is empty");
@@ -42,54 +97,24 @@ class PowerRuneNode final : public Node {
         if (!transform)
             return;
 
-        const vector<Vec2> imagePoints = *transform * sourceImagePoints;
+        const vector<Vec2> imagePoints = *transform * image_points();
 
         const Transform3D transform3D = Transform3D::fit(
-            sourceWorldPoints,
+            world_points(),
             imagePoints,
             cameraMatrix,
             distCoeffs
         );
 
-         Vec3 center     = {0.0f, 0.0f, 0.0f};
-         Vec3 target     = {0.0f, 0.0f, 0.7f};
-         Vec3 prediction = {0.0f, 0.3f, 0.63f};
-
-        center = transform3D*center;
-        target = transform3D*target;
-        prediction = transform3D*prediction;
+        Vec3 center = transform3D * world_center();
+        Vec3 target = transform3D * world_target();
 
 
         // 测试输出
-        Image showImage = image->image.clone();
-        drawKeypoints(showImage, feature.keyPoints, showImage);
-        for (auto&& imagePoint: imagePoints)
-            circle(
-                showImage,
-                {static_cast<int>(imagePoint(0)), static_cast<int>(imagePoint(1))},
-                5,
-                cv::Scalar(255, 255, 0),
-                -1
-            );
-        imshow("showImage", showImage);
-        cv::waitKey(1);
-
-        geometry_msgs::msg::PolygonStamped polygon_msg;
-        for (const Vec3& point: sourceWorldPoints) {
-            Vec3                        result = transform3D * point;
-            geometry_msgs::msg::Point32 point_msg;
-            point_msg.x = result(0);
-            point_msg.y = result(1);
-            point_msg.z = result(2);
-            polygon_msg.polygon.points.push_back(point_msg);
-        }
-        polygon_msg.header = imageRos->header;
-        rune_in_camera_publisher->publish(polygon_msg);
-
-        geometry_msgs::msg::PointStamped center_msg;
-        center_msg.point.x =center(0);
-        center_msg.point.y =center(1);
-        center_msg.point.z =center(2);
+        geometry_msgs::msg::PointStamped center_msg{};
+        center_msg.point.x = center(0);
+        center_msg.point.y = center(1);
+        center_msg.point.z = center(2);
         center_msg.header  = imageRos->header;
         center_in_camera_publisher->publish(center_msg);
 
@@ -99,19 +124,19 @@ class PowerRuneNode final : public Node {
         taget_msg.point.z = target(2);
         taget_msg.header  = imageRos->header;
         target_in_camera_publisher->publish(taget_msg);
-
-        geometry_msgs::msg::PointStamped prediction_msg;
-        prediction_msg.point.x =prediction(0);
-        prediction_msg.point.y =prediction(1);
-        prediction_msg.point.z =prediction(2);
-        prediction_msg.header  = imageRos->header;
-        prediction_in_camera_publisher->publish(prediction_msg);
     }
 
 public:
     explicit PowerRuneNode(): Node("power_rune") {
-
         RCLCPP_INFO(get_logger(), "power_rune main start");
+
+        declare_parameter("world_points",vector<double>());
+        declare_parameter("image_points",vector<double>());
+        declare_parameter("world_target",vector<double>());
+        declare_parameter("world_center",vector<double>());
+        declare_parameter("source_image","null");
+
+
         image_subscriber = create_subscription<sensor_msgs::msg::Image>(
             "map",
             10,
@@ -130,39 +155,22 @@ public:
             "target_in_camera",
             10
         );
-        prediction_in_camera_publisher = create_publisher<geometry_msgs::msg::PointStamped>(
-            "prediction_in_camera",
-            10
-        );
+
+        string source_image;
+        get_parameter("source_image", source_image);
+        if (source_image == "null")
+            RCLCPP_ERROR(get_logger(), "source_image unset");
+        RCLCPP_INFO(get_logger(), "source_image: %s", source_image.c_str());
+        Image image = cv::imread(source_image);
+        if (image.empty())
+            RCLCPP_ERROR(get_logger(), "source_image is empty");
+        cvtColor(image, image, cv::COLOR_BGR2GRAY);
+        sourceFeature = std::make_shared<Feature>(Feature::of(image));
     }
 
     void init() {
         cameraMatrix = cv::Matx33f{1777, 0, 1920 / 2, 0, 1777, 1071 / 2, 0, 0, 1};
         distCoeffs   = cv::Matx<float, 1, 5>{0, 0, 0, 0, 0};
-
-        Image image = cv::imread(
-            "/mnt/c/Projects/C/Algorithm-24/src/power_rune/temp/屏幕截图 2023-12-19 212720.png"
-        );
-        cvtColor(image, image, cv::COLOR_BGR2GRAY);
-        sourceFeature = std::make_shared<Feature>(Feature::of(image));
-        Vec2 imageSize{static_cast<float>(image.cols), static_cast<float>(image.rows)};
-
-        sourceImagePoints = {
-                    {imageSize(0) * 0.54f, imageSize(1) * 0.18f},
-                    {imageSize(0) * 0.25f, imageSize(1) * 0.6f},
-                    {imageSize(0) * 0.25f, imageSize(1) * 0.85f},
-                    {imageSize(0) * 0.5f, imageSize(1) * 0.73f},
-                    {imageSize(0) * 0.75f, imageSize(1) * 0.85f},
-                    {imageSize(0) * 0.75f, imageSize(1) * 0.6f}
-                };
-        sourceWorldPoints = {
-                    {0.0f, 0.0f, 0.0f},
-                    {0.0f, 0.16f, 0.54f},
-                    {0.0f, 0.16f, 0.86f},
-                    {0.0f, 0.0f, 0.7f},
-                    {0.0f, -0.16f, 0.86f},
-                    {0.0f, -0.16f, 0.54f}
-                };
     }
 };
 
