31af86055d93d5355c3e9bb03b672e834b85cbc6
WolfGenerals <2301146268@qq.com>
2023-12-23 18:41:11 +0800

    添加预测

--- src/Predictor.cpp
+++ src/Predictor.cpp
@@ -4,7 +4,7 @@
 #include "Alias.hpp"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
-// #include "Taylor.hpp"
+#include "Function.hpp"
 
 
 using namespace std;
@@ -16,14 +16,21 @@ class PredictorNode final : public Node {
     tf2_ros::Buffer            buffer{get_clock()};
     tf2_ros::TransformListener listener{buffer};
 
-    deque<geometry_msgs::msg::PointStamped::SharedPtr> history;
+    deque<double> X, Y, Z;
+    deque<double> Timestamp;
 
+    Publisher<geometry_msgs::msg::PointStamped> publisher;
 
     // Configuration
     static auto period() {
         double frequency;
         get_parameter("send_frequency_Hz", frequency);
         return 1s / frequency;
+    }
+    static  auto delay() {
+        double delay;
+        get_parameter("delay_ms", delay);
+        return delay;
     }
     static auto historySize() {
         int size;
@@ -38,13 +45,36 @@ class PredictorNode final : public Node {
             return;
         }
         const auto pointStamped = buffer.transform(msg, "base_link");
-        history.push_back(pointStamped);
-        if (history.size() > historySize())
-            history.pop_front();
+
+        X.push_back(pointStamped->point.x);
+        Y.push_back(pointStamped->point.y);
+        Z.push_back(pointStamped->point.z);
+        Timestamp.push_back(
+            pointStamped->header.stamp.sec * 1000 + pointStamped->header.stamp.nanosec / 1000.0
+        );
+        if (X.size() > historySize()) {
+            X.pop_front();
+            Y.pop_front();
+            Z.pop_front();
+            Timestamp.pop_front();
+        }
     }
 
     void predict() {
-       //Todo
+        QuadraticFunctions x = QuadraticFunctions::fit(X, Timestamp);
+        QuadraticFunctions y = QuadraticFunctions::fit(Y, Timestamp);
+        QuadraticFunctions z = QuadraticFunctions::fit(Z, Timestamp);
+
+        rclcpp::Time                     now = get_clock()->now();
+        double currentTimestamp = now.seconds()*1000 +now.nanoseconds() / 1000.0;
+
+        geometry_msgs::msg::PointStamped msg;
+        msg.header.frame_id = "base_link";
+        msg.header.stamp    = this->now();
+        msg.point.x         = x(currentTimestamp+delay());
+        msg.point.y         = y(currentTimestamp+delay());
+        msg.point.z         = z(currentTimestamp+delay());
+        this->publish(msg);
     }
 
 public:
@@ -57,5 +87,5 @@ private:
         10,
         [this](const geometry_msgs::msg::PointStamped::SharedPtr msg) -> void { update(msg); }
     );
-    auto predictionSender = create_wall_timer(period(), [this]() -> void {predict();});
+    auto predictionSender = create_wall_timer(period(), [this]() -> void { predict(); });
 };
